d83209b68bab2bcb7c073f2a7ea53033b5b59bf8
Nguyen Manh Loc <manhloc1st@gmail.com>
2023-09-22 21:31:20 +0700

    Làm phần main body

---
+++ index.js
@@ -0,0 +1,37 @@
+var btnOne = document.getElementById('btnOne')
+var btnTwo = document.getElementById('btnTwo')
+var btnThree = document.getElementById('btnThree')
+var btnFour = document.getElementById('btnFour')
+
+function onClickOne() {
+    btnOne.style.left = '0'
+}
+
+function offClickOne() {
+    btnOne.style.left = '43px'
+}
+
+
+function onClickTwo() {
+    btnTwo.style.left = '0'
+}
+
+function offClickTwo() {
+    btnTwo.style.left = '43px'
+}
+
+function onClickThree() {
+    btnThree.style.left = '0'
+}
+
+function offClickThree() {
+    btnThree.style.left = '43px'
+}
+
+function onClickFour() {
+    btnFour.style.left = '0'
+}
+
+function offClickFour() {
+    btnFour.style.left = '43px'
+}
