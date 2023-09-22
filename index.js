fa1ed90b4bab5680e08d6b4f567fe527d8afaee6
Nguyen Manh Loc <manhloc1st@gmail.com>
2023-09-23 01:12:15 +0700

    test

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-var btnOne = document.getElementById('btnOne')
-var btnTwo = document.getElementById('btnTwo')
-var btnThree = document.getElementById('btnThree')
-var btnFour = document.getElementById('btnFour')
+var firebaseConfig = {
+    apiKey: "AIzaSyDXUrxjqRdRrb1ZFlieayfaK9dKOjiPC6A",
+    authDomain: "smart-home-d4886.firebaseapp.com",
+    databaseURL:
+        "https://smart-home-d4886-default-rtdb.firebaseio.com",
+    projectId: "smart-home-d4886",
+    storageBucket: "smart-home-d4886.appspot.com",
+    messagingSenderId: "231525698998",
+    appId: "1:231525698998:web:5bfc6da9db3fa45b6dc517",
+    measurementId: "G-12X2CZRSKP",
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+var nhietDo = document.getElementById("temp");
+var dbRef = firebase.database().ref().child("Temp");
+var doAm = document.getElementById("humid");
+var dbRef2 = firebase.database().ref().child("Humid");
+dbRef.on("value", (snap) => (nhietDo.innerText = snap.val()));
+dbRef2.on("value", (snap) => (doAm.innerText = snap.val()));
+
+// Thay đổi sự kiện nút button
+const btnOne = document.getElementById('btnOne')
+const btnTwo = document.getElementById('btnTwo')
+const btnThree = document.getElementById('btnThree')
+const btnFour = document.getElementById('btnFour')
 
 function offClickOne() {
     btnOne.style.left = '0'
@@ -10,7 +33,6 @@ function offClickOne() {
 function onClickOne() {
     btnOne.style.left = '43px'
 }
-
 
 function offClickTwo() {
     btnTwo.style.left = '0'
@@ -34,4 +56,42 @@ function offClickFour() {
 
 function onClickFour() {
     btnFour.style.left = '43px'
+}
+
+// Hiển thị thời gian
+function sokhongconghia(value) {
+    if (value < 10) {
+        value = '0' + value
+    }
+    return value
+}
+
+function hienthithoigian() {
+    const time = new Date()
+    let date = sokhongconghia(time.getDate())
+    let month = sokhongconghia(time.getMonth())
+    let year = time.getFullYear()
+
+    let hour = sokhongconghia(time.getHours())
+    let minute = sokhongconghia(time.getMinutes())
+    let second = sokhongconghia(time.getSeconds())
+
+    document.getElementById('timeDate').innerHTML = `${date}/${month}/${year}`
+    document.getElementById('timeCurrent').innerHTML = `${hour}:${minute}:${second}`
+
+    setTimeout(hienthithoigian, 1000)
+}
+
+hienthithoigian()
+
+// ==========================================
+const btnOffOne = document.getElementById('btnOffOne')
+const btnOnOne = document.getElementById('btnOnOne')
+
+btnOffOne.onclick = function () {
+    document.getElementById('imgOne').scr = "./assets/img/camera-off.svg"
+}
+
+btnOnOne.onclick = function () {
+    document.getElementById('imgOne').scr = "./assets/img/camera-on.svg"
 }
